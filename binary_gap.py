6b78f6736746d0c25d7ad551e1278534224159c3
the-timoye <thetimoye@gmail.com>
2021-07-24 13:44:26 +0100

    feat(binary_gap): count gaps

--- binary_gap.py
+++ binary_gap.py
@@ -5,3 +5,27 @@ def convert_to_binary(num, cache):
   cache = str(digit % 2) + cache
   digit = digit // 2
   return convert_to_binary(digit, cache)  
+
+def count_gaps(binary_string):
+  counter = 0
+  index = 0
+  highest_count = counter
+  while index <  len(binary_string):
+    if binary_string[index] == '0':
+      counter += 1
+    else:
+      if highest_count < counter:
+        highest_count = counter
+      counter = 0
+    index+=1
+  return highest_count
+
+def binary_gap(num, cache):
+  if (type(num) != int):
+    return f"{num} should be an integer"
+  binary_string = convert_to_binary(num, cache)
+  return binary_gap(binary_string)
+
+
+if __name__ == '__main__':
+  binary_gap(103, '')
